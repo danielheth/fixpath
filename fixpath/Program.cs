ff0a79aa4a1efe85e39edc908312f5a0ab30f7e4
Daniel Moran <daniel.moran@tanium.com>
2016-06-21 15:01:42 -0500

    initial commit

---
+++ fixpath/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cygfixpath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arg = string.Join(" ", args);
+            Console.Write(arg.Replace("/","\\"));
+        }
+    }
+}
